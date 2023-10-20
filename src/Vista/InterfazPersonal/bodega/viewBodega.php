4a90417ff110e6c4d48364b2393ab61b58a9b703
JosePu99 <joseluis-pusam@cunoc.edu.gt>
2023-10-19 22:51:04 -0600

    cambios de pantalla en html y funciones javascriot

--- src/Vista/InterfazPersonal/bodega/viewBodega.php
+++ src/Vista/InterfazPersonal/bodega/viewBodega.php
@@ -7,16 +7,16 @@
 </head>
 <body>
     <div id="principal">
-        <h1 style="text-align: center;">Bodega Grande</h1>
+        <h1 style="text-align: center;">Area de bodega</h1>
 
         <div id="botones">
-            <button onclick="mostrarFormulario()">Agregar Producto</button>
-            <button onclick="cambiarContenido('Contenido 2')">Modificar Producto</button>
-            <button onclick="cambiarContenido('Contenido 3')">Listar Producto</button>
+            <button onclick="mostrarFormulario('formulario')">Agregar Producto</button>
+            <button onclick="mostrarFormulario('modificarForm')">Modificar Producto</button>
+            <button onclick="listarProductos()">Listar Producto</button>
         </div>
         <div id="contenido-dinamico">
             <div id="resultado"></div>
-            <div id="formulario">
+            <div id="formulario" style="display: none;">
                 <label for="nombre">Nombre del Producto:</label>
                 <input type="text" id="nombre" placeholder="Ingrese el nombre del producto">
 
@@ -26,25 +26,83 @@
                 <label for="precio">Precio:</label>
                 <input type="number" id="precio" placeholder="Ingrese el precio">
 
-                <label for="imagen">Enlace de Imagen:</label>
-                <input type="text" id="imagen" placeholder="Ingrese el enlace de la imagen">
+                <label for="imagen">Imagen de referencia:</label>
+                <input type="text" id="imagen" placeholder="Ingrese el enlace de la imagen de referencia">
 
                 <button onclick="agregarProducto()">Agregar</button>
+            </div>
+            <div id="modificarForm" style="display: none;">
+                <label for="codigo">Código del Producto:</label>
+                <input type="text" id="codigo" placeholder="Ingrese el código del producto">
+                <label for="nuevoNombre">Nuevo Nombre:</label>
+                <input type="text" id="nuevoNombre" placeholder="Ingrese el nuevo nombre">
+                <label for="nuevoPrecio">Nuevo Precio:</label>
+                <input type="number" id="nuevoPrecio" placeholder="Ingrese el nuevo precio">
+                <label for="nuevoEnlace">Nuevo Enlace de Imagen:</label>
+                <input type="text" id="nuevoEnlace" placeholder="Ingrese el nuevo enlace de la imagen">
+                <button onclick="modificarProducto()">Modificar</button>
+            </div>
+            <div id="tablaProductos" style="display: none;">
+                <!-- Tabla para listar productos -->
+                <table id="tabla">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 
     <script>
-        function mostrarFormulario() {
-            document.getElementById('formulario').style.display = 'block';
-            limpiarResultado(); // Agregamos esta línea para limpiar el resultado
+        function mostrarFormulario(formulario) {
+            if (formulario === 'formulario') {
+                document.getElementById('formulario').style.display = 'block';
+                document.getElementById('modificarForm').style.display = 'none';
+                document.getElementById('tablaProductos').style.display= 'none';
+            } else if (formulario === 'modificarForm') {
+                document.getElementById('modificarForm').style.display = 'block';
+                document.getElementById('formulario').style.display = 'none';
+                document.getElementById('tablaProductos').style.display= 'none';
+            }
+            limpiarResultado();
         }
+        function listarProductos() {
 
-        function cambiarContenido(contenido) {
-            document.getElementById('formulario').style.display = 'none';
-            document.getElementById('resultado').innerText = contenido;
+            var productos = [
+                // Aquí debes tener un arreglo con los datos de tus productos
+                { codigo: '001', nombre: 'Producto 1', cantidad: 10, precio: 25.99 },
+                { codigo: '002', nombre: 'Producto 2', cantidad: 5, precio: 19.99 },
+                // Agrega más productos
+            ];
+
+            var tabla = document.querySelector('#tablaProductos table tbody');
+            tabla.innerHTML = '';
+
+            productos.forEach(function(producto) {
+                var fila = document.createElement('tr');
+                fila.innerHTML = `
+                    <td>${producto.codigo}</td>
+                    <td>${producto.nombre}</td>
+                    <td>${producto.cantidad}</td>
+                    <td>${producto.precio}</td>
+                `;
+                tabla.appendChild(fila);
+            });
+
+            var formAgregar = document.getElementById('formulario');
+            var formModificar = document.getElementById('modificarForm');
+            formAgregar.style.display = 'none';
+            formModificar.style.display = 'none';
+            document.getElementById('tablaProductos').style.display = 'block';
         }
-
         function agregarProducto() {
             var nombre = document.getElementById('nombre').value;
             var cantidad = document.getElementById('cantidad').value;
@@ -57,18 +115,23 @@
                 <p>Precio: ${precio}</p>
                 <p>Imagen:</p>
                 <p> <img src="${imagen}" alt="${nombre}" style="max-width: 100px;"></p>`;
-
             document.getElementById('resultado').innerHTML = resultado;
-
+            limpiarFormulario();
             document.getElementById('formulario').style.display = 'none';
-            limpiarFormulario(); // Agregamos esta línea para limpiar el formulario
+        }
+
+        function modificarProducto() {
+            var codigo = document.getElementById('codigo').value;
+            var nuevoNombre = document.getElementById('nuevoNombre').value;
+            var nuevoPrecio = document.getElementById('nuevoPrecio').value;
+            var nuevoEnlace = document.getElementById('nuevoEnlace').value;
+            // Aquí deberías implementar la lógica para modificar el producto con el código proporcionado
         }
 
         function limpiarResultado() {
             document.getElementById('resultado').innerHTML = '';
         }
-
-        function limpiarFormulario() {
+                function limpiarFormulario() {
             document.getElementById('nombre').value = '';
             document.getElementById('cantidad').value = '';
             document.getElementById('precio').value = '';
@@ -77,3 +140,4 @@
     </script>
 </body>
 </html>
+
