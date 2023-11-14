b84abd1d76de3ee79ecdbf8e47535815c04a886c
Dou2020 <doueduar2018@gmail.com>
2023-11-13 19:16:00 -0600

    add script 1.2

--- script.sql
+++ script.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE minishop;
+
+SHOW minishop;
+
 CREATE TABLE `administrador` (
   `id` int(7) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Nombre` varchar(30) NOT NULL,
@@ -8,7 +12,7 @@ INSERT INTO `administrador` (`id`, `Nombre`, `Clave`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 CREATE TABLE `categoria` (
-  `CodigoCat` varchar(30) NOT NULL,
+  `CodigoCat` varchar(30) NOT NULL PRIMARY KEY,
   `Nombre` varchar(30) NOT NULL,
   `Descripcion` text NOT NULL
 );
@@ -25,22 +29,51 @@ CREATE TABLE `cliente` (
 );
 
 CREATE TABLE `cuentabanco` (
-  `id` int(50) NOT NULL PRIMARY KEY,
+  `id` int(50) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `NumeroCuenta` varchar(100) NOT NULL,
   `NombreBanco` varchar(100) NOT NULL,
   `NombreBeneficiario` varchar(100) NOT NULL,
   `TipoCuenta` varchar(100) NOT NULL
 );
 
+CREATE TABLE `venta` (
+  `NumPedido` int(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `Fecha` varchar(150) NOT NULL,
+  `NIT` varchar(30) NOT NULL,
+  `TotalPagar` decimal(30,2) NOT NULL,
+  `Estado` varchar(150) NOT NULL,
+  `NumeroDeposito` varchar(50) NOT NULL,
+  `TipoEnvio` varchar(37) NOT NULL,
+  `Adjunto` varchar(50) NOT NULL,
+  FOREIGN KEY (`NIT`) REFERENCES cliente(`NIT`) ON UPDATE CASCADE
+);
+
+ALTER TABLE `venta`
+  ADD KEY `NIT` (`NIT`),
+  ADD KEY `NIT_2` (`NIT`);
+
 CREATE TABLE `detalle` (
   `NumPedido` int(20) NOT NULL,
   `CodigoProd` varchar(30) NOT NULL,
   `CantidadProductos` int(20) NOT NULL,
-  `PrecioProd` decimal(30,2) NOT NULL
+  `PrecioProd` decimal(30,2) NOT NULL,
+  FOREIGN KEY (`NumPedido`) REFERENCES venta(`NumPedido`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+ALTER TABLE `detalle`
+  ADD KEY `NumPedido` (`NumPedido`),
+  ADD KEY `CodigoProd` (`CodigoProd`);
+
+CREATE TABLE `proveedor` (
+  `NITProveedor` varchar(30) NOT NULL PRIMARY KEY,
+  `NombreProveedor` varchar(30) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `PaginaWeb` varchar(30) NOT NULL
 );
 
 CREATE TABLE `producto` (
-  `id` int(20) NOT NULL PRIMARY KEY,
+  `id` int(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `CodigoProd` varchar(30) NOT NULL,
   `NombreProd` varchar(30) NOT NULL,
   `CodigoCat` varchar(30) NOT NULL,
@@ -52,25 +85,12 @@ CREATE TABLE `producto` (
   `NITProveedor` varchar(30) NOT NULL,
   `Imagen` varchar(150) NOT NULL,
   `Nombre` varchar(30) NOT NULL,
-  `Estado` varchar(15) NOT NULL
+  `Estado` varchar(15) NOT NULL,
+  FOREIGN KEY (`CodigoCat`) REFERENCES categoria(`CodigoCat`) ON UPDATE CASCADE,
+  FOREIGN KEY (`NITProveedor`) REFERENCES proveedor(`NITProveedor`) ON UPDATE CASCADE
 );
 
-CREATE TABLE `proveedor` (
-  `NITProveedor` varchar(30) NOT NULL,
-  `NombreProveedor` varchar(30) NOT NULL,
-  `Direccion` varchar(200) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  `PaginaWeb` varchar(30) NOT NULL
-);
-
-CREATE TABLE `venta` (
-  `NumPedido` int(20) NOT NULL,
-  `Fecha` varchar(150) NOT NULL,
-  `NIT` varchar(30) NOT NULL,
-  `TotalPagar` decimal(30,2) NOT NULL,
-  `Estado` varchar(150) NOT NULL,
-  `NumeroDeposito` varchar(50) NOT NULL,
-  `TipoEnvio` varchar(37) NOT NULL,
-  `Adjunto` varchar(50) NOT NULL
-);
-
+ALTER TABLE `producto`
+  ADD KEY `CodigoCat` (`CodigoCat`),
+  ADD KEY `NITProveedor` (`NITProveedor`),
+  ADD KEY `Agregado` (`Nombre`);
