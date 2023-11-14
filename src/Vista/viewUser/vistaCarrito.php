9009983f1c34e6e476237aa7e6f8196a8af50beb
JosePu99 <joseluis-pusam@cunoc.edu.gt>
2023-11-14 01:45:30 -0600

    solucion de rutas

--- src/Vista/viewUser/vistaCarrito.php
+++ src/Vista/viewUser/vistaCarrito.php
@@ -5,7 +5,7 @@
     <?php include './inc/link.php'; ?>
 </head>
 <body id="container-page-index">
-    <?php include '../Vista/references/navigationBar.php'; //No estoy seguro de que esto funcione por el path
+    <?php include '../references/navigationBar.php'; //No estoy seguro de que esto funcione por el path
      
     ?>
     <section id="container-pedido">
@@ -15,46 +15,56 @@
             </div>
             <br><br><br>
             <div class="row">
-                <div class="col-xs-12">
+            <div class="col-xs-12">
                     <?php
-                     
-                    require_once "src/Vista/viewUser/carrito-compras-logica.php";
-                         
-                    if (!empty($productos_carrito)) {
+                   
+                    include "../Modelo/configServer.php";
+                    include "../Modelo/consulSQL.php";
+                    
+                    if (!empty($_SESSION['carro'])) {
+                        $suma = 0;
+                        $sumaA = 0;
                         echo '<table class="table table-bordered table-hover"><thead><tr class="bg-success"><th>Nombre</th><th>Precio</th><th>Cantidad</th><th>Subtotal</th><th>Acciones</th></tr></thead>';
-                        foreach ($productos_carrito as $producto) {
-                            echo "<tbody>
-                                    <tr>
-                                        <td>" . $producto['nombre'] . "</td>
-                                        <td> " . $producto['precio'] . "</td>
-                                        <td> " . $producto['cantidad'] . "</td>
-                                        <td> " . $producto['subtotal'] . "</td>
-                                        <td>
-                                            <form action='process/quitarproducto.php' method='POST' class='FormCatElec' data-form=''>
-                                                <input type='hidden' value='" . $codeProd['producto'] . "' name='codigo'>
-                                                <button class='btn btn-danger btn-raised btn-xs'>Eliminar</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                </tbody>";
+                        foreach ($_SESSION['carro'] as $codeProd) {
+                            $consulta = ejecutarSQL::consultar("SELECT * FROM producto WHERE CodigoProd='" . $codeProd['producto'] . "'");
+                            while ($fila = mysqli_fetch_array($consulta, MYSQLI_ASSOC)) {
+                                $pref = number_format(($fila['Precio'] - ($fila['Precio'] * ($fila['Descuento'] / 100))), 2, '.', '');
+                                echo "<tbody>
+                                            <tr>
+                                                <td>" . $fila['NombreProd'] . "</td>
+                                                <td> " . $pref . "</td>
+                                                <td> " . $codeProd['cantidad'] . "</td>
+                                                <td> " . $pref * $codeProd['cantidad'] . "</td>
+                                                <td>
+                                                    <form action='process/quitarproducto.php' method='POST' class='FormCatElec' data-form=''>
+                                                        <input type='hidden' value='" . $codeProd['producto'] . "' name='codigo'>
+                                                        <button class='btn btn-danger btn-raised btn-xs'>Eliminar</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        </tbody>";
+                                $suma += $pref * $codeProd['cantidad'];
+                                $sumaA += $codeProd['cantidad'];
+                            }
+                            mysqli_free_result($consulta);
                         }
                         echo '<tr class="bg-danger"><td colspan="2">Total</td><td><strong>' . $sumaA . '</strong></td><td><strong>$' . number_format($suma, 2) . '</strong></td></tr></table><div class="ResForm"></div>';
                         echo '
-                        <p class="text-center">
-                        <a href="product.php" class="btn btn-primary btn-raised btn-lg">Seguir comprando</a>
-                        <a href="process/vaciarcarrito.php" class="btn btn-success btn-raised btn-lg">Vaciar el carrito</a>
-                        <a href="pedido.php" class="btn btn-danger btn-raised btn-lg">Confirmar el pedido</a>
-                        </p>
-                        ';
+                            <p class="text-center">
+                            <a href="product.php" class="btn btn-primary btn-raised btn-lg">Seguir comprando</a>
+                            <a href="process/vaciarcarrito.php" class="btn btn-success btn-raised btn-lg">Vaciar el carrito</a>
+                            <a href="pedido.php" class="btn btn-danger btn-raised btn-lg">Confirmar el pedido</a>
+                            </p>
+                            ';
                     } else {
-                        echo '<p class="text-center text-danger lead">El carrito de compras está vacío</p><br>
-                        <a href="product.php" class="btn btn-primary btn-lg btn-raised">Ir a Productos</a>';
+                        echo '<p class="text-center text-danger lead">El carrito de compras esta vacío</p><br>
+                            <a href="product.php" class="btn btn-primary btn-lg btn-raised">Ir a Productos</a>';
                     }
                     ?>
                 </div>
             </div>
         </div>
     </section>
-    <?php include './inc/footer.php'; ?>
+    <?php include '../references/footer.php'; ?>
 </body>
 </html>
